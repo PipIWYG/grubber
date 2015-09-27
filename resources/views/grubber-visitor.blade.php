8cf1052d560d5bc6cae48e53122f40de8e18aa15
Quintin Stoltz <pip@pip.co.za>
2015-09-27 09:15:51 +0200

    Added Environment Based FavIcons to indicate Environment

--- resources/views/grubber-visitor.blade.php
+++ resources/views/grubber-visitor.blade.php
@@ -6,6 +6,65 @@
 	</div>
 @endsection
 
+@section('favicon')
+	@if (env('APP_ENV') == "local")
+		<link rel="apple-touch-icon" sizes="57x57" href="/favico-dev-apple-touch-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="/favico-dev-apple-touch-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="/favico-dev-apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="/favico-dev-apple-touch-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="/favico-dev-apple-touch-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="/favico-dev-apple-touch-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="/favico-dev-apple-touch-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="/favico-dev-apple-touch-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="/favico-dev-apple-touch-icon-180x180.png">
+		<link rel="icon" type="image/png" href="/favico-dev-favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="/favico-dev-android-chrome-192x192.png" sizes="192x192">
+		<link rel="icon" type="image/png" href="/favico-dev-favicon-96x96.png" sizes="96x96">
+		<link rel="icon" type="image/png" href="/favico-dev-favicon-16x16.png" sizes="16x16">
+		<link rel="manifest" href="/favico-dev-manifest.json">
+		<meta name="msapplication-TileColor" content="#da532c">
+		<meta name="msapplication-TileImage" content="/favico-dev-mstile-144x144.png">
+		<meta name="theme-color" content="#ffffff">
+	@elseif (env('APP_ENV') == "staging" || env('APP_ENV') == "test")
+		<link rel="apple-touch-icon" sizes="57x57" href="/favico-acpt-apple-touch-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="/favico-acpt-apple-touch-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="/favico-acpt-apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="/favico-acpt-apple-touch-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="/favico-acpt-apple-touch-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="/favico-acpt-apple-touch-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="/favico-acpt-apple-touch-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="/favico-acpt-apple-touch-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="/favico-acpt-apple-touch-icon-180x180.png">
+		<link rel="icon" type="image/png" href="/favico-acpt-favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="/favico-acpt-android-chrome-192x192.png" sizes="192x192">
+		<link rel="icon" type="image/png" href="/favico-acpt-favicon-96x96.png" sizes="96x96">
+		<link rel="icon" type="image/png" href="/favico-acpt-favicon-16x16.png" sizes="16x16">
+		<link rel="manifest" href="/favico-acpt-manifest.json">
+		<meta name="msapplication-TileColor" content="#da532c">
+		<meta name="msapplication-TileImage" content="/favico-acpt-mstile-144x144.png">
+		<meta name="theme-color" content="#ffffff">
+	@else
+		<link rel="apple-touch-icon" sizes="57x57" href="/apple-touch-icon-57x57.png">
+		<link rel="apple-touch-icon" sizes="60x60" href="/apple-touch-icon-60x60.png">
+		<link rel="apple-touch-icon" sizes="72x72" href="/apple-touch-icon-72x72.png">
+		<link rel="apple-touch-icon" sizes="76x76" href="/apple-touch-icon-76x76.png">
+		<link rel="apple-touch-icon" sizes="114x114" href="/apple-touch-icon-114x114.png">
+		<link rel="apple-touch-icon" sizes="120x120" href="/apple-touch-icon-120x120.png">
+		<link rel="apple-touch-icon" sizes="144x144" href="/apple-touch-icon-144x144.png">
+		<link rel="apple-touch-icon" sizes="152x152" href="/apple-touch-icon-152x152.png">
+		<link rel="apple-touch-icon" sizes="180x180" href="/apple-touch-icon-180x180.png">
+		<link rel="icon" type="image/png" href="/favicon-32x32.png" sizes="32x32">
+		<link rel="icon" type="image/png" href="/android-chrome-192x192.png" sizes="192x192">
+		<link rel="icon" type="image/png" href="/favicon-96x96.png" sizes="96x96">
+		<link rel="icon" type="image/png" href="/favicon-16x16.png" sizes="16x16">
+		<link rel="manifest" href="/manifest.json">
+		<meta name="msapplication-TileColor" content="#da532c">
+		<meta name="msapplication-TileImage" content="/mstile-144x144.png">
+		<meta name="theme-color" content="#ffffff">
+	@endif
+	
+@endsection
+
 <!DOCTYPE html>
 <html lang="en">
 <head>
@@ -14,8 +73,7 @@
 	<meta name="viewport" content="width=device-width, initial-scale=1">
 	<title>{{env('APP_NAME')}}</title>
 	
-	<link href="{{ asset('favicon.ico') }}" type="image/x-icon" rel="icon"/>
-    <link href="{{ asset('favicon.ico') }}" type="image/x-icon" rel="shortcut icon"/>
+	@yield('favicon')
 	
 	<link href='//fonts.googleapis.com/css?family=Archivo+Narrow' rel='stylesheet' type='text/css'>
 	<link rel="stylesheet" href="//code.jquery.com/ui/1.11.4/themes/smoothness/jquery-ui.css">
